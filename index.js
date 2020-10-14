8a0aec6a0495827fe625f501d72add1a4f46b1a7
kiyoung <popping2606@gmail.com>
2020-10-14 21:04:38 +0900

    js: addItem()

--- index.js
+++ index.js
@@ -1,26 +1,47 @@
 const lists = [
   {
+    _id: 1,
     text: 'first item',
     done: false,
   },
   {
+    _id: 2,
     text: 'second item',
     done: true,
   },
 ]
 
-const list = document.querySelector('.lists')
+let length = lists.length
 
-//리스트 그리기
-let html = ''
-if(lists.length > 0){
-  for(let i of lists){
-    html += `<div class="item">
-      <div class="done hover ${i.done}"></div>
-      <div class="text">${i.text}</div>
-      <button class="button edit hover">edit</button>
-      <button class="button delete hover">delete</button>
-    </div>`
+function refresh() {
+  console.log('refresh')
+  //리스트 그리기
+  let html = ''
+  for (let i of lists) {
+    html += `<div class="item ${i.done}">
+    <div class="done hover ${i.done}"></div>
+    <div class="text ${i.done}">${i.text}</div>
+    <button class="button edit hover">수정</button>
+    <button class="button delete hover">삭제</button>
+  </div>`
   }
-  if(list) list.innerHTML = html
+  const list = document.querySelector('.lists')
+  if (list) list.innerHTML = html
 }
+
+function addItem() {
+  length++
+  lists.push({
+    _id: length,
+    text: '',
+    done: false,
+  })
+}
+
+window.addEventListener('load', function () {
+  refresh()
+  document.querySelector('.add').onclick = function(){
+    addItem()
+    refresh()
+  }
+})
