1822cad71428c84fac5d753abe81043a00817399
kiyoung <popping2606@gmail.com>
2020-10-16 18:16:27 +0900

    fix resizeTextarea()

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let lists = [
   {
     _id: 2,
     text: 'second item',
-    done: true,
+    done: false,
   },
 ]
 
@@ -32,9 +32,10 @@ function refresh() {
 
   //add 이벤트 추가
   const add = document.querySelector('.add')
-  if(add) add.onclick = function () {
-    addItem()
-  }
+  if (add)
+    add.onclick = function () {
+      addItem()
+    }
 
   //done 이벤트 추가
   const dones = document.querySelectorAll('.done')
@@ -58,8 +59,19 @@ function refresh() {
     i.onchange = function () {
       changeText(Number(i.parentNode.parentNode.id.replace('item_', '')))
     }
+    i.onkeyup = function() {
+      resizeTextarea(Number(i.parentNode.parentNode.id.replace('item_', '')))
+    }
+    i.onkeydown = function() {
+      resizeTextarea(Number(i.parentNode.parentNode.id.replace('item_', '')))
+    }
+    i.onfocusout = function() {
+      resizeTextarea(Number(i.parentNode.parentNode.id.replace('item_', '')))
+    }
   }
+  resizeTextarea()
 }
+// 변경시 리스트를 다시 렌더링하는 함수 //
 
 /* 이벤트 함수들 생성 */
 //add버튼 클릭하면 item 추가하고 focus
@@ -83,21 +95,44 @@ function deleteItem(_id) {
 
 //done 클릭하면 item의 스타일 변경하고 refresh()
 function toggleDone(_id) {
-  console.log(_id)
   lists = lists.map((i) => (i._id === _id ? { ...i, done: !i.done } : i))
   refresh()
 }
 
 //textarea 변경되면 lists 변경
 function changeText(_id) {
-  const textEl = document.querySelector(`#item_${_id}`)
-  let text = ''
-  if (textEl) text = textEl.querySelector('textarea').value
-  console.log(text)
+  const text = document.querySelector(`#item_${_id} textarea`).value
   lists = lists.map((i) => (i._id === _id ? { ...i, text: text } : i))
 }
 
+//textarea를 찾아서 크기를 글자 높이에 맞게 변경하기
+function resizeTextarea(_id) {
+  const fake = document.querySelector('#fakeTextarea')
+  if (_id && fake) {
+    const textarea = document.querySelector(`#item_${_id} textarea`)
+    if (textarea) {
+      fake.style.height = '0px'
+      fake.style.width = textarea.clientWidth + 'px'
+      fake.value = textarea.value
+      textarea.style.height = fake.scrollHeight + 'px'
+      fake.value = ''
+      fake.style.height = '0px'
+    }
+    return
+  }
 
+  const textareas = document.querySelectorAll('textarea')
+  if (fake) {
+    for (let i = 0; i < textareas.length-1; i++) {
+      fake.style.height = '0px'
+      fake.style.width = textareas[i].clientWidth + 'px'
+      fake.value = textareas[i].value
+      textareas[i].style.height = 8 + fake.scrollHeight + 'px'
+    }
+    fake.value = ''
+    fake.style.height = '0px'
+  }
+}
 
 //로드 완료 후 리프레시
 window.addEventListener('load', function () {
